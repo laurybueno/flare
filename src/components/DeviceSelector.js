214541fbf63c1603270e089de91befbf3d39f5d9
Laury Bueno <laury.bueno@guiabolso.com.br>
2022-03-22 23:30:25 -0300

    Add MUI Components and use a Select for device selection

--- src/components/DeviceSelector.js
+++ src/components/DeviceSelector.js
@@ -1,32 +1,50 @@
 import { useEffect, useState } from 'react';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import { FormControl, InputLabel } from '@mui/material';
 
 function DeviceSelector({ setMediaStream, ...props }) {
+  const [device, setDevice] = useState("");
   const [devices, setDevices] = useState([]);
 
   const getDevices = async () => {
     return await navigator.mediaDevices.enumerateDevices();
   }
 
-  const getStream = async event => {
-    const mediaStream = await navigator.mediaDevices.getUserMedia({
-      "video": {
-        "deviceId": event.target.value,
-      }
-    });
-    setMediaStream(mediaStream);
-  }
+  const updateDevice = event => {
+    setDevice(event.target.value);
+  };
 
   useEffect(() => {
     getDevices().then(ds => setDevices(ds));
   }, []);
 
+  useEffect(() => {
+    const getStream = async () => {
+      const mediaStream = await navigator.mediaDevices.getUserMedia({
+        "video": {
+          "deviceId": device,
+        }
+      });
+      setMediaStream(mediaStream);
+    };
+    if (device)
+      getStream(); 
+  }, [device]);
+
   return (
-      <select onChange={getStream}>
-        <option>Select webcam</option>
+    <FormControl fullWidth>
+      <InputLabel>Select webcam</InputLabel>
+      <Select 
+        label="Select webcam"
+        value={device}
+        onChange={updateDevice}
+      >
         { devices.filter(d => d.kind === "videoinput").map(d => {
-          return <option key={d.deviceId} value={d.deviceId} >{d.label}</option>
+          return <MenuItem key={d.deviceId} value={d.deviceId}>{d.label}</MenuItem>
         }) }
-      </select>
+      </Select>
+    </FormControl>
   )
 }
 
